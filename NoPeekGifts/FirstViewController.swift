4ffe5de67fe7287ce64953569a4fe56cf00ad5a0
Mark Gruetzner <mgruetzner@rw3.com>
2015-10-31 15:30:49 -0500

    Initial Commit

---
+++ NoPeekGifts/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NoPeekGifts
+//
+//  Created by Mark Gruetzner on 10/31/15.
+//  Copyright © 2015 XNI Technologies, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
