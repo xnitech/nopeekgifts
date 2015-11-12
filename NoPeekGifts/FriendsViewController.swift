e94f16117bbae6573391f7c609e7219e0adcab5c
Mark Gruetzner <mark.gruetzner@gmail.com>
2015-11-11 21:27:43 -0600

    Added fb login

--- NoPeekGifts/FriendsViewController.swift
+++ NoPeekGifts/FriendsViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class FriendsViewController: UIViewController {
+class FriendsViewController: UIViewController, FBSDKLoginButtonDelegate {
 
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
+    
+    if (FBSDKAccessToken.currentAccessToken() == nil)
+    {
+        print("Not logged in")
+    }
+    else {
+        print("Logged in")
+    }
+    
+    let loginButton = FBSDKLoginButton()
+    loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+    loginButton.center = self.view.center
+    loginButton.delegate = self
+    
+    self.view.addSubview(loginButton)
    }
 
    override func didReceiveMemoryWarning() {
@@ -20,6 +37,20 @@ class FriendsViewController: UIViewController {
       // Dispose of any resources that can be recreated.
    }
 
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if (error == nil)
+        {
+            print("Login complete")
+        }
+        else {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User logged out")
+    }
+    
 
 }
 
